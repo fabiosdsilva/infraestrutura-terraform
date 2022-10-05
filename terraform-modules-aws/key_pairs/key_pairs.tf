75abe6ed0fbd0e891aa0c0506db4182dc78b45b2
fabiosdsilva <fabiosds17@gamail.com>
2022-10-04 22:33:30 -0300

    chore: Correcao na criacao da cahve privada tls

--- terraform-modules-aws/key_pairs/key_pairs.tf
+++ terraform-modules-aws/key_pairs/key_pairs.tf
@@ -1,4 +1,6 @@
 resource "tls_private_key" "tsl_key_pair" {
+  count = length(var.key_name)
+
   algorithm = "RSA"
   rsa_bits  = 4096
 }
@@ -7,7 +9,7 @@ resource "aws_key_pair" "key_pair" {
   count      = length(var.key_name)
 
   key_name   = var.key_name[count.index]
-  public_key = tls_private_key.tsl_key_pair.public_key_openssh
+  public_key = tls_private_key.tsl_key_pair[count.index].public_key_openssh
 }
 
 resource "aws_secretsmanager_secret" "key-web-server" {
@@ -20,5 +22,5 @@ resource "aws_secretsmanager_secret_version" "secret_kms" {
   count         = length(var.key_name)
 
   secret_id     = aws_secretsmanager_secret.key-web-server[count.index].id
-  secret_string = "${tls_private_key.tsl_key_pair.private_key_pem}"
+  secret_string = "${tls_private_key.tsl_key_pair[count.index].private_key_pem}"
 }
